0a64563a0a2a5b3f45387a6ac6afc10f6a17e9db
georgemakrakis <georgemakrakis88@gmail.com>
2017-04-05 13:57:55 +0300

    Model for Thesis Form added

---
+++ AegeanThesis/Models/ThesisForm.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AegeanThesis.Models
+{
+    public class ThesisForm
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Supervisor { get; set; }
+
+        [Range(1,3)]
+        public string NumStudents { get; set; }
+
+        public string Purpose { get; set; }
+        public string Description { get; set; }
+
+        public string [] PrereqLessons { get; set; }
+
+        public string PrereqKnowledge { get; set; }
+
+        //Στοιχεία Φοιτητών - Foregin key?
+
+        [Display(Name = "Announcement Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public string AnnouncDate { get; set; }
+
+        [Display(Name = "Adoption Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public string AdoptionDate { get; set; }
+
+        [Display(Name = "Finish Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public string FinishDate { get; set; }
+
+        [Range(5, 10)]
+        public float Grade { get; set; }
+    }
+}
